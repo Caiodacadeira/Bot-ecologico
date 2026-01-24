0cf4c4c74e32786f83e0398819fc8554450cf174
Caiodacadeira <caiospinasse18@gmail.com>
2026-01-24 11:30:43 -0300

    update main.py

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import discord
-import random
-import os
-import requests
+from bot_logic import RS
 from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='r!', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.command()
-async def Rs(ctx, R:int):
-  await ctx.send()
+async def Rs(ctx, R: int):
+  await ctx.send(RS(R))
 
 bot.run('token')
