c06ccbee53ea0b81d3cc2c4baa9c96b9bf91b119
Caiodacadeira <caiospinasse18@gmail.com>
2026-01-30 21:10:25 -0300

    Add ajuda command for help on rs command
    Added a new command 'ajuda' to provide help information about the 'rs' command.

--- main.py
+++ main.py
@@ -11,4 +11,15 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 async def rs(ctx, R: int):
   await ctx.send(rs_funcao(R))
 
+@bot.command()
+async def ajuda(ctx):
+  ajuda_texto = (
+    "🌿 números disponíveis no comando rs:\n"
+    "1-10: Retorna a explicação do R correspondente.\n"
+    "11: Explica a existência dos Rs da sustentabilidade.\n"
+    "12: Mostra todos os Rs da sustentabilidade.\n\n"
+    "Exemplo de uso: !rs 3"
+  )
+  await ctx.send(ajuda_texto)
+
 bot.run('token')
